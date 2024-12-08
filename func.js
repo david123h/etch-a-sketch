b8e22f4aab6f07a086d169d9362432db53817801
david123h <hilelit4321@gmail.com>
2024-12-08 12:55:21 +0200

    grid and event listener were created

--- func.js
+++ func.js
@@ -1,12 +1,23 @@
-
 function addSqueres(givenNumber) {
   for (let i = 0; i < givenNumber ** 2; i++) {
+
+    // Calculate the box width 
+    const cubeWidthAndHeight = 550 / givenNumber ;
+
     const box = document.getElementById(`container`);
 
-const squre = document.createElement(`div`);
-squre.classList.add(`squre`);
+    const squre = document.createElement(`div`);
+    squre.classList.add(`squre`);
+    squre.style.height = `${cubeWidthAndHeight}px`
+    squre.style.width = `${cubeWidthAndHeight}px`
     box.appendChild(squre);
+
+    squre.addEventListener(`mouseover`, () =>
+      {  squre.style.backgroundColor = `red`
+      })
   }
 }
 
-addSqueres(8);
+
+
+addSqueres(200);
