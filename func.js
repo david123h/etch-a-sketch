0aa7ecb250a849ea19886ba12ecc175a215c2e0d
david123h <hilelit4321@gmail.com>
2024-12-08 22:22:14 +0200

    the RESET button is working

--- func.js
+++ func.js
@@ -1,23 +1,29 @@
+const box = document.getElementById(`container`);
+
 function addSqueres(givenNumber) {
   for (let i = 0; i < givenNumber ** 2; i++) {
-
-    // Calculate the box width 
-    const cubeWidthAndHeight = 550 / givenNumber ;
-
-    const box = document.getElementById(`container`);
+    // Calculate the box width
+    const cubeWidthAndHeight = 550 / givenNumber;
 
     const squre = document.createElement(`div`);
     squre.classList.add(`squre`);
-    squre.style.height = `${cubeWidthAndHeight}px`
-    squre.style.width = `${cubeWidthAndHeight}px`
+    squre.style.height = `${cubeWidthAndHeight}px`;
+    squre.style.width = `${cubeWidthAndHeight}px`;
     box.appendChild(squre);
 
-    squre.addEventListener(`mouseover`, () =>
-      {  squre.style.backgroundColor = `red`
-      })
+    squre.addEventListener(`mouseover`, () => {
+      squre.style.backgroundColor = `black`;
+    });
   }
 }
 
+addSqueres(50);
 
+const resetButton = document.getElementById(`reset`);
 
-addSqueres(200);
+resetButton.addEventListener(`click`, () => {
+  const allsqueres = box.children;
+  for (let squre of allsqueres) {
+    squre.style.backgroundColor = `white`;
+  } 
+});
