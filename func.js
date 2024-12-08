7a990ab19eabfbd94ef25a3fbe50d778ac7577c4
david123h <hilelit4321@gmail.com>
2024-12-08 23:06:47 +0200

    COLORFUL button is working, comments added

--- func.js
+++ func.js
@@ -1,25 +1,46 @@
 const box = document.getElementById(`container`);
 
+// adding squares into the cotainers
 function addSqueres(givenNumber) {
-  for (let i = 0; i < givenNumber ** 2; i++) {
-    // Calculate the box width
-    const cubeWidthAndHeight = 550 / givenNumber;
 
+  // Calculate the squre width and height
+  const cubeWidthAndHeight = 550 / givenNumber;
+
+  for (let i = 0; i < givenNumber ** 2; i++) {
+//adding squre to the DOM in each iteration
     const squre = document.createElement(`div`);
     squre.classList.add(`squre`);
     squre.style.height = `${cubeWidthAndHeight}px`;
     squre.style.width = `${cubeWidthAndHeight}px`;
     box.appendChild(squre);
-
+// paint the square when the mouse is hover on it
     squre.addEventListener(`mouseover`, () => {
-      squre.style.backgroundColor = `black`;
+        if (currentColor === `colorful`)
+      {squre.style.backgroundColor = getRandomColor()
+      }else {
+        squre.style.backgroundColor = `black`;
+      };
     });
   }
 }
 
-addSqueres(50);
+addSqueres(10);
 
+//catching buttons
 const resetButton = document.getElementById(`reset`);
+const blackButton = document.getElementById(`black`)
+const sizeButton = document.getElementById(`size`)
+const colorfulButton = document.getElementById(`colorful`)
+
+//for the first time the color is black
+let currentColor = `black`
+
+//Event listeners for the buttons
+colorfulButton.addEventListener(`click`, () =>
+currentColor = `colorful`)
+
+blackButton.addEventListener(`click`, () =>
+currentColor = `black`)
 
 resetButton.addEventListener(`click`, () => {
   const allsqueres = box.children;
@@ -27,3 +48,11 @@ resetButton.addEventListener(`click`, () => {
     squre.style.backgroundColor = `white`;
   } 
 });
+
+//return random color function 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256); // Random red value (0-255)
+  const g = Math.floor(Math.random() * 256); // Random green value (0-255)
+  const b = Math.floor(Math.random() * 256); // Random blue value (0-255)
+  return `rgb(${r}, ${g}, ${b})`; // Return the RGB color as a string
+}
